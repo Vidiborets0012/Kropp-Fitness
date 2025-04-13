9371d9ea6b354e217e5cff7b2ca0beaf62421b56
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-04-13 14:19:45 +0300

    add banner section styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const swiper = new Swiper('.banner__slider', {
-  loop: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
 
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 });
